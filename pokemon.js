6d2a9c51df4238401655acccabe26cf96776c655
Carlos Santa Maria O <cssantam@gmail.com>
2020-05-14 18:07:40 -0400

    Optimizacion de codigo

--- pokemon.js
+++ pokemon.js
@@ -11,106 +11,9 @@ $(document).ready(function () {
         nombre = nombre.toLowerCase();
         //alert("buscar ajax");
 
-        $.ajax({
-            url: `https://pokeapi.co/api/v2/pokemon/${nombre}`,
-            type: "get",
-            dataType: 'json',
-        })
-        .done(function (result) {
-            var pokeID = result.id;
-            var nombre = result.name;
-            var imagen = `https://pokeres.bastionbot.org/images/pokemon/${pokeID}.png`;
-            var types = result.types;
-            var altura = result.height;
-            var peso = result.weight;
-            var genero = result.genre;
-            var color = result.color;
-            var stats = result.stats;
-            var statsLabel = [];
-            var statsValues = [];
-            var specie = result.species.name;
-
-            var listaTipos = "<ul>";
-            types.forEach(function (type) {
-                listaTipos += "<li>" + type['type']['name'] + "</li>";
-            });
-            listaTipos += "</lu>";
-
-            for (i = 0; i < stats.length; i++) {
-                statsLabel[i] = stats[i].stat.name;
-                statsValues[i] = stats[i].base_stat;
-            }
-
-
-
-            var tarjeta = '<div class="card">';
-            tarjeta += `<img src="${imagen}" class="card-img-top" alt="..." style="padding:20px;">
-            <div class="card-body">
-                <h5 class="card-title">Nro: ${pokeID}</h5>
-                <hr>
-                <h5 class="card-title">${nombre}</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                    content. This content is a little bit longer.</p>
-                    <hr>    
-                    <table border='1' width="30%" padding="5px"; border-radius="5px"; >
-                        <tr align="center" style=""><td>Tipos</td></tr>
-                        <tr align="center"><td>${listaTipos}</td></tr>
-                    </table>
-                    <table border="1" padding="5px"; width="80%";>
-                    <hr>
-                        <tr align="center" style=""><td>Altura</td><td>Peso</td><td>Especie</td></tr>
-                        <tr align="center"><td>${altura}</td><td>${peso}</td><td>${specie}</td></tr>
-                    </table>
-                    <hr>
-                    <canvas id="myChart" width="400" height="200"></canvas>
-
-            </div></div>`;
-
-            $("#unPokemon").html(tarjeta);
-            var ctx = document.getElementById('myChart').getContext('2d');
-            var chart = new Chart(ctx, {
-                // The type of chart we want to create
-                type: 'horizontalBar',
-
-                // The data for our dataset
-
-                //https://www.pokexperto.net/index2.php?seccion=nds/nationaldex/stats&pk=3
-                data: {
-                    //labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                    labels: statsLabel,
-                    datasets: [{
-                        label: '',
-                        backgroundColor: 'rgb(255, 99, 132)',
-                        borderColor: 'rgb(255, 99, 132)',
-                        data: statsValues
-                    }]
-                },
-
-                // Configuration options go here
-                options: {}
-            });
-            
-            $('.carousel').hide();
-            $(".gifAnimado").hide();
-            $("#poke-container").hide();
-            $(".info-group").hide();
-            $(".unPokemon").show();
-            
-        });
+        showCardPokemon( nombre);
     });
 });
-
-/*
-$('#txtBuscar').keypress(function(event){
-    var keycode = (event.keyCode ? event.keyCode : event.which);
-    if(keycode == '13'){
-
-        alert('You pressed a "enter" key in textbox'); 
-
-    }
-    event.stopPropagation();
-});
-*/
 
 
 function inicio() {
@@ -125,92 +28,8 @@ function getSelectPokemon(sel) {
     
     var nombre = sel;
 
-    $.ajax({
-            url: `https://pokeapi.co/api/v2/pokemon/${nombre}`,
-            type: "get",
-            dataType: 'json',
-    })
-    .done(function (result) {
-        var pokeID = result.id;
-        var nombre = result.name;
-        var imagen = `https://pokeres.bastionbot.org/images/pokemon/${pokeID}.png`;
-        var types = result.types;
-        var altura = result.height;
-        var peso = result.weight;
-        var genero = result.genre;
-        var color = result.color;
-        var stats = result.stats;
-        var statsLabel = [];
-        var statsValues = [];
-        var specie = result.species.name;
+    showCardPokemon( nombre);
 
-        var listaTipos = "<ul>";
-        types.forEach(function (type) {
-            listaTipos += "<li>" + type['type']['name'] + "</li>";
-        });
-        listaTipos += "</lu>";
-
-        for (i = 0; i < stats.length; i++) {
-            statsLabel[i] = stats[i].stat.name;
-            statsValues[i] = stats[i].base_stat;
-        }
-
-
-
-        var tarjeta = '<div class="card">';
-        tarjeta += `<img src="${imagen}" class="card-img-top" alt="..." style="padding:20px;">
-        <div class="card-body">
-            <h5 class="card-title">Nro: ${pokeID}</h5>
-            <hr>
-            <h5 class="card-title">${nombre}</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                content. This content is a little bit longer.</p>
-                <hr>    
-                <table border="1" style="padding:5px;" width="30%">
-                    <tr align="center" style=""><td>Tipos</td></tr>
-                    <tr align="center"><td>${listaTipos}</td></tr>
-                </table>
-                <table border="1" style="padding:5px;" width="80%">
-                <hr>
-                    <tr align="center" style=""><td>Altura</td><td>Peso</td><td>Especie</td></tr>
-                    <tr align="center"><td>${altura}</td><td>${peso}</td><td>${specie}</td></tr>
-                </table>
-                <hr>
-                <canvas id="myChart" width="400" height="200"></canvas>
-
-        </div></div>`;
-
-        $("#unPokemon").html(tarjeta);
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var chart = new Chart(ctx, {
-            // The type of chart we want to create
-            type: 'horizontalBar',
-
-            // The data for our dataset
-
-            //https://www.pokexperto.net/index2.php?seccion=nds/nationaldex/stats&pk=3
-            data: {
-                //labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                labels: statsLabel,
-                datasets: [{
-                    label: '',
-                    backgroundColor: 'rgb(255, 99, 132)',
-                    borderColor: 'rgb(255, 99, 132)',
-                    data: statsValues
-                }]
-            },
-
-            // Configuration options go here
-            options: {}
-        });
-        $('.carousel').hide();
-        $(".gifAnimado").hide();
-        $(".info-group").hide();
-        $("#poke-container").hide();
-        $("#tabla").hide();
-        $(".unPokemon").show();
-        
-    });
 }
 
 
@@ -249,93 +68,8 @@ function csFunction(){
         nombre = nombre.toLowerCase();
         //alert("buscar ajax");
 
-        $.ajax({
-            url: `https://pokeapi.co/api/v2/pokemon/${nombre}`,
-            type: "get",
-            dataType: 'json',
-        })
-        .done(function (result) {
-            var pokeID = result.id;
-            var nombre = result.name;
-            var imagen = `https://pokeres.bastionbot.org/images/pokemon/${pokeID}.png`;
-            var types = result.types;
-            var altura = result.height;
-            var peso = result.weight;
-            var genero = result.genre;
-            var color = result.color;
-            var stats = result.stats;
-            var statsLabel = [];
-            var statsValues = [];
-            var specie = result.species.name;
-
-            var listaTipos = "<ul>";
-            types.forEach(function (type) {
-                listaTipos += "<li>" + type['type']['name'] + "</li>";
-            });
-            listaTipos += "</lu>";
-
-            for (i = 0; i < stats.length; i++) {
-                statsLabel[i] = stats[i].stat.name;
-                statsValues[i] = stats[i].base_stat;
-            }
-
-
-
-            var tarjeta = '<div class="card">';
-            tarjeta += `<img src="${imagen}" class="card-img-top" alt="..." style="padding:20px;">
-            <div class="card-body">
-                <h5 class="card-title">Nro: ${pokeID}</h5>
-                <hr>
-                <h5 class="card-title">${nombre}</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                    content. This content is a little bit longer.</p>
-                    <hr>    
-                    <table border='1' width="30%" padding="5px"; border-radius="5px"; >
-                        <tr align="center" style=""><td>Tipos</td></tr>
-                        <tr align="center"><td>${listaTipos}</td></tr>
-                    </table>
-                    <table border="1" padding="5px"; width="80%";>
-                    <hr>
-                        <tr align="center" style=""><td>Altura</td><td>Peso</td><td>Especie</td></tr>
-                        <tr align="center"><td>${altura}</td><td>${peso}</td><td>${specie}</td></tr>
-                    </table>
-                    <hr>
-                    <canvas id="myChart" width="400" height="200"></canvas>
-
-            </div></div>`;
-
-            $("#unPokemon").html(tarjeta);
-            var ctx = document.getElementById('myChart').getContext('2d');
-            var chart = new Chart(ctx, {
-                // The type of chart we want to create
-                type: 'horizontalBar',
-
-                // The data for our dataset
-
-                //https://www.pokexperto.net/index2.php?seccion=nds/nationaldex/stats&pk=3
-                data: {
-                    //labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                    labels: statsLabel,
-                    datasets: [{
-                        label: '',
-                        backgroundColor: 'rgb(255, 99, 132)',
-                        borderColor: 'rgb(255, 99, 132)',
-                        data: statsValues
-                    }]
-                },
-
-                // Configuration options go here
-                options: {}
-            });
-            
-            $('.carousel').hide();
-            $(".gifAnimado").hide();
-            $(".info-group").hide();
-            $("#poke-container").hide();
-            $("#tabla").hide();
-            $(".unPokemon").show();
-            
-        });
+        showCardPokemon( nombre);
+    
     }
 }
 
@@ -412,7 +146,94 @@ const escribeTabla = (pokemon) => {
                             <td> ${pokemon.name} </td>
                             <td> ${pokemon.url}</td>
                             <td><button class="btn text-light" onclick="getSelectPokemon('${pokemon.name}')">'${pokemon.name}'</button></td></tr>`
-   
-           
+}
 
+
+function showCardPokemon( nombreId) {
+    $.ajax({
+        url: `https://pokeapi.co/api/v2/pokemon/${nombre}`,
+        type: "get",
+        dataType: 'json',
+    })
+    .done(function (result) {
+        var pokeID = result.id;
+        var nombre = result.name;
+        var imagen = `https://pokeres.bastionbot.org/images/pokemon/${pokeID}.png`;
+        var types = result.types;
+        var altura = result.height;
+        var peso = result.weight;
+        var genero = result.genre;
+        var color = result.color;
+        var stats = result.stats;
+        var statsLabel = [];
+        var statsValues = [];
+        var specie = result.species.name;
+
+        var listaTipos = "<ul>";
+        types.forEach(function (type) {
+            listaTipos += "<li>" + type['type']['name'] + "</li>";
+        });
+        listaTipos += "</lu>";
+
+        for (i = 0; i < stats.length; i++) {
+            statsLabel[i] = stats[i].stat.name;
+            statsValues[i] = stats[i].base_stat;
+        }
+
+
+
+        var tarjeta = '<div class="card">';
+        tarjeta += `<img src="${imagen}" class="card-img-top" alt="..." style="padding:20px;">
+        <div class="card-body">
+            <h5 class="card-title">Nro: ${pokeID}</h5>
+            <hr>
+            <h5 class="card-title">${nombre}</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+                content. This content is a little bit longer.</p>
+                <hr>    
+                <table border='1' width="30%" padding="5px"; border-radius="5px"; >
+                    <tr align="center" style=""><td>Tipos</td></tr>
+                    <tr align="center"><td>${listaTipos}</td></tr>
+                </table>
+                <table border="1" padding="5px"; width="80%";>
+                <hr>
+                    <tr align="center" style=""><td>Altura</td><td>Peso</td><td>Especie</td></tr>
+                    <tr align="center"><td>${altura}</td><td>${peso}</td><td>${specie}</td></tr>
+                </table>
+                <hr>
+                <canvas id="myChart" width="400" height="200"></canvas>
+
+        </div></div>`;
+
+        $("#unPokemon").html(tarjeta);
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'horizontalBar',
+
+            // The data for our dataset
+
+            //https://www.pokexperto.net/index2.php?seccion=nds/nationaldex/stats&pk=3
+            data: {
+                //labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                labels: statsLabel,
+                datasets: [{
+                    label: '',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: statsValues
+                }]
+            },
+
+            // Configuration options go here
+            options: {}
+        });
+        
+        $('.carousel').hide();
+        $(".gifAnimado").hide();
+        $("#poke-container").hide();
+        $(".info-group").hide();
+        $(".unPokemon").show();
+        
+    });
 }
